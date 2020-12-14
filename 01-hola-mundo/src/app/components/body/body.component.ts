71bf4ed0b2899e0907653cad6d3fb038556ba41f
Angel Abraham <angel-07061@hotmail.com>
2020-12-14 17:05:49 -0600

    Commit Footer

---
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -0,0 +1,11 @@
+import { from } from "rxjs";
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'    
+})
+
+export class BodyComponent {
+
+};
