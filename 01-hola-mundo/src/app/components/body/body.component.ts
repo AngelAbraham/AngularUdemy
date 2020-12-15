9f0f4f9f28a484691ca7913bd9aae31eeb3d61b4
Angel Abraham <angel-07061@hotmail.com>
2020-12-15 01:28:24 -0600

    Commit ngIf ngFoe

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -8,4 +8,15 @@ import { Component } from '@angular/core';
 
 export class BodyComponent {
 
+    mostrar = true;
+
+    frase: any={
+        mensaje:"Un gran poder con lleva una gran responsabilidad",
+        autor:"Ben Parker"
+    };
+
+    personajes: string[]= ['Spiderman','Venom','Carnage'];
+
+
+
 };
